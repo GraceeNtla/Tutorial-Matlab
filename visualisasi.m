25aa3cb42a28e4e4671286b70a64042c7d386ab7
Grace <164855707+GraceeNtla@users.noreply.github.com>
2024-04-25 11:05:07 +0700

    Add files via upload

--- visualisasi.m
+++ visualisasi.m
@@ -63,3 +63,85 @@
 % mesh(x,y,z);
 % grid on
 
+% f1
+% x = [0:0.001:1];
+% y = (sin(5*pi*x)).^6;
+% plot(x,y,'b')
+% xlabel('sumbu x')
+% ylabel('f(x)')
+% title('Grafik Fungsi (sin(5*pi*x)).^6')
+% legend('f(x)')
+% grid on
+
+% f2
+% x = [0:0.001:1];
+% y = exp(-2*log(2)*((x-0.1)/0.8).^2).*(sin(5*pi*x)).^6;
+% plot(x,y,'r-')
+% xlabel('sumbu x')
+% ylabel('f(x)')
+% title('Grafik Fungsi exp(-2*log(2)*((x-0.1)/0.8).^2).*(sin(5*pi*x)).^6')
+% legend('f(x)')
+% grid on
+
+% f3
+% x = -10:0.01:10;
+% y = -10:0.01:10;
+% [X,Y] = meshgrid(x,y);
+% Z = (X.^2+Y-11).^2+(X+Y.^2-7).^2;
+% meshc(X,Y,Z)
+% xlabel('sumbu x')
+% ylabel('sumbu y')
+% zlabel ('sumbu f(x)')
+% title('Grafik Fungsi (x.^2+y-11).^2+(x+y.^2-7).^2')
+% grid on
+
+% f4
+% x = -4:0.1:4;
+% y = -4:0.1:4;
+% [X,Y] = meshgrid(x,y);
+% Z = 0.5*(X.^4-16*X.^2+5*X)+0.5*(Y.^4-16*Y.^2+5*Y);
+% figure (1)
+% contour3(X,Y,Z)
+% figure (2)
+% meshc(X,Y,Z)
+% xlabel('sumbu x')
+% ylabel('sumbu y')
+% zlabel ('sumbu z')
+% title('Grafik Fungsi 0.5*(X.^4-16*X.^2+5*X)+0.5*(Y.^4-16*Y.^2+5*Y)')
+% grid on
+
+% f4
+% x = -1.9:0.1:1.9;
+% y = -1.1:0.1:1.1;
+% [X,Y] = meshgrid(x,y);
+% Z = (4-2.1*X.^2+X.^4/3).*X.^2+X.*Y+4*(-1+Y.^2).*Y.^2;
+% figure (1)
+% contour3(X,Y,Z)
+% figure (2)
+% meshc(X,Y,Z)
+% figure (3)
+% surfc (X,Y,Z)
+% xlabel('sumbu x')
+% ylabel('sumbu y')
+% zlabel ('sumbu z')
+% title('Grafik Fungsi (4-2.1*X.^2+X.^4/3).*X.^2+X.*Y+4*(-1+Y.^2).*Y.^2')
+% grid on
+
+%f7
+% x = -10:0.1:10;
+% y = -10:0.1:10;
+% [X,Y] = meshgrid(x,y);
+% Z = sin(X).*exp((1-cos(Y)).^2)+cos(Y).*exp((1-cos(Y)).^2)+(X-Y).^2;
+% figure (1)
+% contour3(X,Y,Z)
+% figure (2)
+% meshc(X,Y,Z)
+% figure (3)
+% surf(X,Y,Z)
+% xlabel('sumbu x')
+% ylabel('sumbu y')
+% zlabel ('sumbu z')
+% title('Grafik Fungsi (4-2.1*X.^2+X.^4/3).*X.^2+X.*Y+4*(-1+Y.^2).*Y.^2')
+% grid on
+
+
