b51a839e564db7ec36b0a421a8ff0b2b75b0e967
Grace <164855707+GraceeNtla@users.noreply.github.com>
2024-05-06 15:52:23 +0700

    Add files via upload

---
+++ Jacobi_2.m
@@ -0,0 +1,47 @@
+% Jacobi Tanpa Fungsi
+% A = [5 9; 3 -6];
+% b = [5; 4];
+% x0 = zeros(size(b));
+% tol = 1e-6;
+% max_iter =  100;
+% 
+% n = length(b);  % panjang matriks
+% x = x0; % inisialisasi nilai awal
+% iter = 0; % inisialisasi iterasi awal
+% error = inf; % set error sebagai di awal perhitungan
+% 
+% % input
+% % A = Matriks koefisien
+% % b = Matriks Hasil
+% % x0 = nilai awal = 0
+% % tol = batas toleransi
+% % max_iter = maksimum iterasi
+% 
+% % output
+% % nilai x
+% % iterasi
+% 
+% while error > tol && iter < max_iter
+%     x_old = x;
+%     for i = 1:n
+%         sigma = 0;
+%         for j = 1:n
+%             if j ~= i
+%             sigma = sigma + A(i,j)*x_old(j);
+%             end
+%         end
+%     x(i) = (b(i)-sigma)/A(i,i);
+%     end 
+% error = norm(x-x_old);
+% iter = iter+i;
+% end
+% if iter == max_iter && error > tol
+%     disp('Iterasi Maksimum tercapai');
+% else 
+%     disp('metode jacobi berakhir di', num2str(iter),'iterasi');
+% end
+% 
+% disp('Solusi:');
+% disp(x);
+
+
--- run_jacobi.m
+++ run_jacobi.m
@@ -1,7 +1,8 @@
-A = [5 9; 3 -6];
-b = [5 ; 4];
-x0 = zeros(size(b));
-tol = 1e-6;
-maxiter =  100;
-(x,iter)= jacobi(A,b,maxiter,x0,tol);
-disp(x);
+% A = [5 9; 3 -6];
+% b = [5; 4];
+% x0 = zeros(size(b));
+% tol = 1e-6;
+% max_iter =  100;
+% [x,iter]= jacobi(A,b,x0,tol,max_iter);
+% disp('Solusi:');
+% disp(x);
